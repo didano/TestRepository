7f9d1807976b0c1f8e8a81c958380c3c78c76133
Vladislav Kernychnyi <bastard290@gmail.com>
2020-10-13 10:57:53 +0300

    First Commit

---
+++ app/src/main/java/com/android/uraall/counter/MainActivityViewModel.java
@@ -0,0 +1,29 @@
+package com.android.uraall.counter;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+
+    private int count = 1;
+    private MutableLiveData<Integer> countLiveData = new MutableLiveData<>();
+
+    //Changed
+    public void getDecreasedValue(){
+        int i = 0;
+        --count;
+        countLiveData.setValue(count);
+    }
+
+    public void getIncreasedValue(){
+        ++count;
+        countLiveData.setValue(count);
+    }
+
+    public MutableLiveData<Integer> getCurrentValue(){
+        countLiveData.setValue(count);
+        return countLiveData;
+    }
+
+}
+
